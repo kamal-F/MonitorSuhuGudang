65af6b2ee7e5dbc2dfe6b0799be1315d2c42bcfa
kamal-F <kamalonnetx@gmail.com>
2025-07-06 06:27:11 +0700

    Initial commit

---
+++ src/main/java/com/example/monitorsuhugudang/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.monitorsuhugudang
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btn = findViewById<Button>(R.id.btnStatus)
+        btn.setOnClickListener {
+            Toast.makeText(this, "Suhu gudang stabil", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
